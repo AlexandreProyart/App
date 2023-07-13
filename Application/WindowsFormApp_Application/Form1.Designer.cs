68296633d5452729063195957a5ac9051bcb9196
AlexandreProyart <alexandre.proyart57@gmail.com>
2023-07-13 19:59:19 +0200

    v1

---
+++ Application/WindowsFormApp_Application/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormApp_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxPseudo = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelPseudo = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connexion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPseudo
+            // 
+            this.textBoxPseudo.Location = new System.Drawing.Point(90, 49);
+            this.textBoxPseudo.Name = "textBoxPseudo";
+            this.textBoxPseudo.Size = new System.Drawing.Size(129, 22);
+            this.textBoxPseudo.TabIndex = 1;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(90, 77);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(129, 22);
+            this.textBoxPassword.TabIndex = 2;
+            // 
+            // labelPseudo
+            // 
+            this.labelPseudo.AutoSize = true;
+            this.labelPseudo.Location = new System.Drawing.Point(12, 49);
+            this.labelPseudo.Name = "labelPseudo";
+            this.labelPseudo.Size = new System.Drawing.Size(54, 16);
+            this.labelPseudo.TabIndex = 3;
+            this.labelPseudo.Text = "Pseudo";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(12, 77);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(67, 16);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelPseudo);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxPseudo);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxPseudo;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelPseudo;
+        private System.Windows.Forms.Label labelPassword;
+    }
+}
+
---
+++ Application/WindowsFormApp_Application/Form1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Data.SqlClient;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormApp_Application
+{
+    public partial class Form1 : Form
+    {
+        public string connectionString = "server=localhost;port=3306;userid=app;password=;database=bdd";
+        public MySqlConnection connection;
+
+        public string pseudoAdminTextbox;
+        public string passwordAdminTextbox;
+        public string pseudoAdminBDD;
+        public string passwordAdminBDD;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                MySqlCommand selectAllUser = new MySqlCommand(); //On crée une nouvelle requête
+                selectAllUser.Connection = connection; //On associe la connexion à la bdd à la requête
+                selectAllUser.CommandText = "SELECT * FROM User"; //On écrit la requête SQL voulue
+                MySqlDataReader reader; //On se prépare à lire les données
+                reader = selectAllUser.ExecuteReader(); //On associe la lecture à la reqûete qu'on a préparé avant
+
+                while (reader.Read()) //Lis les valeurs de la table utilisateurs
+                {
+                        pseudoAdminBDD = Convert.ToString(reader.GetValue(1)); //On récupère la 1ère valeur de la 1ère ligne dans la bdd
+                        passwordAdminBDD = Convert.ToString(reader.GetValue(2)); //On récupère la 2ème valeur de la 1ère ligne dans la bdd
+                }
+                reader.Close(); //On ferme la lecture
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pseudoAdminTextbox = textBoxPseudo.Text;
+            passwordAdminTextbox = textBoxPassword.Text;
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            if (pseudoAdminTextbox == pseudoAdminBDD && passwordAdminTextbox == passwordAdminBDD)
+            {
+                MessageBox.Show("You are connected as an administrator");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+            connection.Close();
+        }
+    }
+}
